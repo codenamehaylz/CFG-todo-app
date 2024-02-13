57520cdb4f1a2d879b2da42a275d9ecea4297ac7
Hayley Parker <hb_060492@hotmail.co.uk>
2024-02-13 15:31:48 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const todos = [];
+
+// { title: string, completed: boolean }
+
+function addTodo() {
+    const inputTodoElement = document.querySelector('#inputTodo');
+    const title = inputTodoElement.value;
+    todos.push({title, completed: false});
+    renderTodos();
+};
+
+function renderTodos() {
+    const todoListElement = document.querySelector('#todoList');
+    todoListElement.innerHTML = "";
+    todos.forEach((todo, index) => {
+        const listElement = document.createElement('li');
+        listElement.innerHTML = `
+            <p>${todo.title}</p>
+            <p>${todo.completed ? "Completed" : "Pending"}</p>
+            <button onclick="toggleTodo(${index})">Toggle</button>
+            <button onclick="deleteTodo(${index})">Delete</button>
+        `;
+        todoListElement.appendChild(listElement);
+    });
+};
+
+// delete todo function
+function deleteTodo(index) {
+    todos.splice(index, 1);
+    renderTodos();
+};
+
+// toggle todo function
+function toggleTodo(index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodos();
+};
+
+
+// 3. Check if there is a value
+// 4. If there is a value, create a new task
